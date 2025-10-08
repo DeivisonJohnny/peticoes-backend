29ee537bac47059f99455c2020950dd0644b6bdd
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-10-08 11:50:41 -0300

    correção no dominio do cors sem /

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://peticoes-worxbase.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://peticoes-worxbase.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
